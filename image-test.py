2d063eccadc47ce87206b814f27ea59cf4c69348
BasStuurwold <34968674+BasStuurwold@users.noreply.github.com>
2020-01-22 00:45:20 +0100

    Text, background and icon fix
    Fixed the positioning of the text
    Added a black filter to the background
    Added a white filter to the icon, so every uploaded icon will be white

--- image-test.py
+++ image-test.py
@@ -7,8 +7,11 @@ class newImage:
     def __init__(self):
         # Set file size in pixel and fonts
         self.W, self.H = (1191,1684)
-        self.fontTitle = ImageFont.truetype('fonts/MYRIADPRO-BOLD.otf', size=220)
-        self.fontBody = ImageFont.truetype('fonts/MYRIADPRO-BOLD.otf', size=100)
+        self.fontsizeTitle = 125
+        self.fontsizeBody = 75
+        self.fontTitle = ImageFont.truetype('fonts/MYRIADPRO-BOLD.otf', size=self.fontsizeTitle)
+        self.fontBody = ImageFont.truetype('fonts/MYRIADPRO-BOLD.otf', size=self.fontsizeBody)
+        self.fontBodyRegular = ImageFont.truetype('fonts/MYRIADPRO-REGULAR.otf', size=100)
 
         # Set inputs
         self.title = input('Hoe heet het evenement? ')
@@ -46,12 +49,22 @@ class newImage:
         self.image2 = self.image2.resize((round((self.W*2)), self.H))
         self.image.paste(self.image2)
 
+        # Paste black filter
+        self.image5 = Image.open('zwart.png').convert('RGBA')
+        w, h = self.image5.size
+        self.image.paste(self.image5, (0, 0), self.image5)
+
         # Paste icon
         self.fetchIcon()
         self.image4 = Image.open('icon.png').convert('RGBA')
-        self.image4 = self.image4.resize((200, 200))
+        self.image4 = self.image4.resize((300, 300))
         w, h = self.image4.size
-        self.image.paste(self.image4, ((round((self.W-w)/2),round((self.H-h)/2-450))), self.image4)
+        self.image.paste(self.image4, ((round((self.W-w)/2),round((self.H-h)/2-200))), self.image4)
+
+        # Paste white mask over icon
+        self.image6 = Image.new('RGBA', (300,300), color='white')
+        w, h = self.image6.size
+        self.image.paste(self.image6, ((round((self.W-w)/2),round((self.H-h)/2-200))), self.image4)
         
     def pasteDecorations(self):
         # Paste logo, socials and gradient.
@@ -62,7 +75,11 @@ class newImage:
     def inputText(self):
         # Putting in the text
         draw = ImageDraw.Draw(self.image)
-        w, h = draw.textsize(self.title)
+        w1, h1 = draw.textsize(self.title, font=self.fontTitle)
+        w, h = draw.textsize(self.date, font=self.fontBody)
+        w, h = draw.textsize(self.time, font=self.fontBody)
+        w, h = draw.textsize(self.adress, font=self.fontBody)
+        w, h = draw.textsize(self.price, font=self.fontBody)
         value = 0
 
         # Set dictionary for inputs
@@ -72,13 +89,14 @@ class newImage:
         words[300] = self.adress
         words[400] = self.price
 
-        draw.text(((self.W-w)/2,(self.H-h)/2), self.title, fill="white", font=self.fontTitle)
+        draw.text(((self.W-w1)/2,(self.H-h1)/2-500), self.title, fill="white", font=self.fontTitle)
 
         # Iterate through dictionary
         for key in words.keys():
             value = value + 100
             print(words[value])
-            draw.text(((self.W-w)/2-500,(self.H-h)/2+value), words[value], fill="white", font=self.fontBody)
+            # draw.text(((self.W-w)/2-450,(self.H-h)/2+value+180), words[value], fill="white", font=self.fontBody)
+            draw.text((75,(self.H-h)/2+value+200), words[value], fill="white", font=self.fontBody)
 
     def saveImage(self):
         self.image.save('test.png')
